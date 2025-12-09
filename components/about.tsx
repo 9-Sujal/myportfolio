2e30ef954416f749eaf8f6d0a3b1ca239a11db64
Sujal Ghorse <143560767+9-Sujal@users.noreply.github.com>
2025-12-09 16:05:36 +0530

    Clean up About component by removing unused code
    Removed unused imports and commented code in the About component.

--- components/about.tsx
+++ components/about.tsx
@@ -2,25 +2,10 @@
 import React from 'react'
 import {motion} from "framer-motion"
 import SectionHeading from './section-heading'
-// import { useInView } from 'react-intersection-observer'
-// import { useActiveSectionContext } from '@/context/active-section-context'
 import { useSectionInView } from '@/lib/hooks'
 export default function About() {
 
   const {ref} = useSectionInView("About", 0.7); 
-  //providing threshold to change only when we scroll till 75%
-//   const {ref,inView} = useInView({
-//     threshold: 0.7, 
-//     rootMargin: "-50px 0px",
-//   });  
-//   const {setActiveSection, timeOfLastClick}= useActiveSectionContext();
-
-  
-//   useEffect(()=>{
-//  if(inView && Date.now()- timeOfLastClick>1000){
-//     setActiveSection("About")
-//   }
-//   }, [inView, setActiveSection, timeOfLastClick])
  
   return (
     <motion.section 
@@ -38,10 +23,10 @@ export default function About() {
             practical and elegant solutions using modern tools and best practices.
     
             During my BTech in Computer Science, I worked on several projects — from full-stack web
-            apps to AI-driven prototypes — where I focused on writing clean, efficient, and
+            apps to  prototypes — where I focused on writing clean, efficient, and
             maintainable code. My frontend design sense helps me craft intuitive user interfaces,
             I&apos;m a quick learner who loves exploring new technologies. My core skills include{' '}
-            <strong>C++, JavaScript, Python, Java, React.js,</strong> and <strong>Node.js</strong>. I am
+            <strong>C++, JavaScript, Next.js,</strong> and <strong>Node.js</strong>. I am
             eager to contribute to impactful projects, learn from experienced teams, and grow as a
             developer.
           </p>
